d6e1532b1b6d3619eb365479a82b63e3833df88b
joaomendoncaa <joaommm0312@gmail.com>
2022-05-24 04:18:28 +0100

    update_direction, vecdeque

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use std::collections::HashSet;
+use std::collections::VecDeque;
 
 pub type Position = (usize, usize);
+
+#[derive(Debug)]
 pub enum Direction {
     Up,
     Down,
@@ -8,10 +10,11 @@ pub enum Direction {
     Right,
 }
 
+#[derive(Debug)]
 pub struct CentipedeGame {
     width: usize,
     height: usize,
-    snake: Vec<Position>,
+    snake: VecDeque<Position>,
     direction: Direction,
     food_position: Position,
 }
@@ -21,9 +24,35 @@ impl CentipedeGame {
         return CentipedeGame {
             width,
             height,
-            snake: vec![((width - 2).max(0), height / 2)],
+            snake: [((width - 2).max(0), height / 2)].into_iter().collect(),
             direction: Direction::Left,
             food_position: (2.min(width - 1), height / 2),
         };
+    }
+
+    pub fn update_direction(&mut self, direction: Direction) {
+        match (&self.direction, direction) {
+            (Direction::Up, Direction::Up)
+            | (Direction::Up, Direction::Down)
+            | (Direction::Down, Direction::Up)
+            | (Direction::Down, Direction::Down)
+            | (Direction::Left, Direction::Left)
+            | (Direction::Left, Direction::Right)
+            | (Direction::Right, Direction::Left)
+            | (Direction::Right, Direction::Right) => {}
+            (_, direction) => self.direction = direction,
+        }
+    }
+
+    pub fn loop_tick(&mut self) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::CentipedeGame;
+
+    #[test]
+    fn test() {
+        println!("{:?}", CentipedeGame::new(10, 10));
     }
 }
